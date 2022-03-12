86e3da97a2db548f82946f56fe17f7e2b836bfc1
hurachima <nakonechniy033@gmail.com>
2022-03-12 17:27:25 +0200

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,48 @@
-
-import Card from "./components/Card";
-import Header from "./components/Header";
-import Drawer from "./components/Drawer";
-import React from "react";
-
-
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+import React from 'react';
 
 function App() {
+  const [items, setItems] = React.useState([]);
 
-  const [items, setItems] = React.useState([])
-
-  const [cartOpened, setCartOpened] = React.useState(false)
-
+  const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
     fetch('https://620bf759ab956ad805680033.mockapi.io/Items')
-    .then((res)=> {
-      return res.json()
-    })
-    .then((json)=>{
-      setItems(json)
-    })
-
-  },[])
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        setItems(json);
+      });
+  }, []);
 
   return (
-    <div className="wrapper clear">
-      {cartOpened ? <Drawer  onClose={()=> setCartOpened(false)}/> : null}
-      <Header onClickCart={()=> setCartOpened(true)} />
+    <div className='wrapper clear'>
+      {cartOpened ? <Drawer onClose={() => setCartOpened(false)} /> : null}
+      <Header onClickCart={() => setCartOpened(true)} />
 
-      <div className="content p-40">
-        <div className="d-flex justify-between align-center mb-40">
+      <div className='content p-40'>
+        <div className='d-flex justify-between align-center mb-40'>
           <h1>Все кроссовки</h1>
-          <div className="search-block d-flex">
-            <img src="/img/search.svg" alt="search" />
-            <input type="search" placeholder="Поиск..." />
+          <div className='search-block d-flex'>
+            <img src='/img/search.svg' alt='search' />
+            <input type='search' placeholder='Поиск...' />
           </div>
         </div>
 
-        <div className="d-flex flex-wrap">
+        <div className='d-flex flex-wrap'>
           {items.map(obj => (
-            <Card 
+            <Card
               name={obj.name}
-              price={obj.price} 
+              price={obj.price}
               imageUrl={obj.imageUrl}
-              onClickFavourite={()=> alert('Нажали на сердечко')} 
-            />)
-          )}
-
+              onClickFavourite={() => alert('Нажали на сердечко')}
+            />
+          ))}
         </div>
-
-
-
       </div>
-
-
     </div>
   );
 }
--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -1,54 +1,73 @@
-import React from 'react'
+import React from 'react';
 
-import styles from './Drawer.module.scss'
+import styles from './Drawer.module.scss';
 
 function Drawer(props) {
   return (
-    <div  className={styles.drawerOwerlay}>
-    <div className={styles.drawer}>
-    <h2 className="d-flex justify-between">Корзинa<img className="removeBtn cu-p" onClick={props.onClose} src="/img/btn-remove.svg" alt="remove" /></h2>
+    <div className={styles.drawerOwerlay}>
+      <div className={styles.drawer}>
+        <h2 className='d-flex justify-between'>
+          Корзинa
+          <img
+            className='removeBtn cu-p'
+            onClick={props.onClose}
+            src='/img/btn-remove.svg'
+            alt='remove'
+          />
+        </h2>
 
-    <div className="items">
-    <div className="cartItem d-flex align-center">
-      <img className="mr-20" width={70} height={70} src="/img/sneakers/1.jpg" alt="sneakers" />
-      <div className="mr-20">
-        <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-          <b>12 999 руб.</b>
+        <div className='items'>
+          <div className='cartItem d-flex align-center'>
+            <img
+              className='mr-20'
+              width={70}
+              height={70}
+              src='/img/sneakers/1.jpg'
+              alt='sneakers'
+            />
+            <div className='mr-20'>
+              <p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className='removeBtn' src='/img/btn-remove.svg' alt='remove' />
+          </div>
+
+          <div className='cartItem d-flex align-center'>
+            <img
+              className='mr-20'
+              width={70}
+              height={70}
+              src='/img/sneakers/1.jpg'
+              alt='sneakers'
+            />
+            <div className='mr-20'>
+              <p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className='removeBtn' src='/img/btn-remove.svg' alt='remove' />
+          </div>
         </div>
-        <img className="removeBtn" src="/img/btn-remove.svg" alt="remove" />
-    </div>
 
-
-    <div className="cartItem d-flex align-center">
-      <img className="mr-20" width={70} height={70} src="/img/sneakers/1.jpg" alt="sneakers" />
-      <div className="mr-20">
-        <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-          <b>12 999 руб.</b>
+        <div className='cartTotalBlock'>
+          <ul>
+            <li>
+              <span>Итого: </span>
+              <div></div>
+              <b>21 498 руб.</b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 руб.</b>
+            </li>
+            <button className='greenButton'>
+              Оформить заказ <img src='/img/arrow.svg' alt='arrow' />
+            </button>
+          </ul>
         </div>
-        <img className="removeBtn" src="/img/btn-remove.svg" alt="remove" />
+      </div>
     </div>
-
-    </div>
-
-    <div className="cartTotalBlock">
-    <ul>
-      <li>
-        <span>Итого: </span>
-        <div></div>
-        <b>21 498 руб.</b>
-      </li>
-      <li>
-        <span>Налог 5%:</span>
-        <div></div>
-        <b>1074 руб.</b>
-      </li>
-      <button className="greenButton">Оформить заказ <img src="/img/arrow.svg" alt="arrow" /></button>
-    </ul>
-    </div>
-
-  </div>
-  </div>
-  )
+  );
 }
 
-export default Drawer
+export default Drawer;
